8b48a995c172c3168075e8d4327f70e0b2cb3d30
Fatemeh Mish Mast <mishmastf293@gmail.com>
2024-05-23 18:17:37 +0330

    Generate_Random_string.py

--- Generate_Random_string.py
+++ Generate_Random_string.py
@@ -1,33 +1,31 @@
 import random
 
 
-def convert_to_string(myList):
-    text = ''
-    for i in myList:
-        text = text + str(i)
-    return text
+def create_password():
+    selected_numbers = []
+    selected_letters = []
+    selected_signs = []
+
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+               "r", "s", "t", "u", "v", "w", "x", "y", "m", "z",
+               "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+               "R", "S", "T", "U", "V", "W", "X", "Y", "M", "Z",
+               ]
+    signs = ["~", "!", "#", "$", "%", "^", "&", "*", "(", ")", "_", "|"]
+    for i in range(5):
+        num = random.choice(numbers)
+        selected_numbers.append(num)
+        letter = random.choice(letters)
+        selected_letters.append(letter)
+        sign = random.choice(signs)
+        selected_signs.append(sign)
+
+    result_list = selected_letters + selected_signs + selected_numbers
+    result_list.pop()
+    random.shuffle(result_list)
+    result_string = ''.join(result_list)
+    return result_string
 
 
-selected_numbers = []
-selected_letters = []
-selected_signs = []
-
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
-           "r", "s", "t", "u", "v", "w", "x", "y", "m", "z",
-           "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
-           "R", "S", "T", "U", "V", "W", "X", "Y", "M", "Z",
-           ]
-signs = ["~", "!", "#", "$", "%", "^", "&", "*", "(", ")", "_", "|"]
-for i in range(5):
-    num = random.choice(numbers)
-    selected_numbers.append(num)
-    letter = random.choice(letters)
-    selected_letters.append(letter)
-    sign = random.choice(signs)
-    selected_signs.append(sign)
-
-result_list = selected_letters + selected_signs + selected_numbers
-result_list.pop()
-random.shuffle(result_list)
-print(convert_to_string(result_list))
+print(create_password())
