b73e3a60177ff80b47ff36c56a0627d1599ea8ac
Fatemeh Mish Mast <mishmastf293@gmail.com>
2024-05-23 15:10:01 +0330

    add Generate_Random_string.py

--- Generate_Random_string.py
+++ Generate_Random_string.py
@@ -1,31 +1,33 @@
 import random
 
 
-def Convert_To_String(myList):
+def convert_to_string(myList):
     text = ''
     for i in myList:
-        text = text + str(i) + ""
+        text = text + str(i)
     return text
 
 
-Selected_numbers = []
-Selected_letters = []
-Selected_signs = []
-Numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-Letters = ["a", "b", "c", "d", " e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+selected_numbers = []
+selected_letters = []
+selected_signs = []
+
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
            "r", "s", "t", "u", "v", "w", "x", "y", "m", "z",
-           "A", "B", "C", "D", " E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+           "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
            "R", "S", "T", "U", "V", "W", "X", "Y", "M", "Z",
            ]
 signs = ["~", "!", "#", "$", "%", "^", "&", "*", "(", ")", "_", "|"]
-for i in range(3):
-    Num = random.choice(Numbers)
-    Selected_numbers.append(Num)
-    Letter = random.choice(Letters)
-    Selected_letters.append(Letter)
-    Sign = random.choice(signs)
-    Selected_signs.append(Sign)
+for i in range(5):
+    num = random.choice(numbers)
+    selected_numbers.append(num)
+    letter = random.choice(letters)
+    selected_letters.append(letter)
+    sign = random.choice(signs)
+    selected_signs.append(sign)
 
-result_list = Selected_letters + Selected_signs + Selected_numbers
+result_list = selected_letters + selected_signs + selected_numbers
+result_list.pop()
 random.shuffle(result_list)
-print(Convert_To_String(result_list))
+print(convert_to_string(result_list))
